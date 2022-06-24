c01daf18834a560b96b3e0716d293ce20e8d9030
Андрей Андрей <KUKASI222@outlook.com>
2022-06-24 18:02:01 +0200

    update access fix + css

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 from .models import Profile
-
 from .forms import UserLoginForm, UserRegistrationForm
 
 
