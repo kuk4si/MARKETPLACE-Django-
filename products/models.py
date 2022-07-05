b199125f3c311b7fe8c5d23cf95aa0f2049102cd
Андрей Андрей <KUKASI222@outlook.com>
2022-07-05 21:27:05 +0200

    product edit fix

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 
 
@@ -37,3 +38,6 @@ class Product(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('products:detail', kwargs={'slug': self.slug})
