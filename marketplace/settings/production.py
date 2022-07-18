511fb21e8f676f373f99f0b5945c09aae5a7e562
Андрей Андрей <KUKASI222@outlook.com>
2022-07-18 22:13:53 +0200

    fix

--- marketplace/settings/production.py
+++ marketplace/settings/production.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 DB_NAME = os.environ.get('DB_NAME')
-DB_PASSWORD = os.environ.get('DB_NAME')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
 # SECRET_KEY = os.environ.get('SECRET_KEY')
