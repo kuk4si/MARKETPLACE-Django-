9807b07339e1aabfe6c2975ecd160f43060d793a
Андрей Андрей <KUKASI222@outlook.com>
2022-07-19 17:45:32 +0200

    media

--- marketplace/settings/production.py
+++ marketplace/settings/production.py
@@ -125,10 +125,34 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+
+# STATIC_URL = 'static/'
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+#
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+#
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#
+# # Основной url для управления медиафайлами
+# MEDIA_URL = '/media/'
+#
+# # Путь хранения картинок
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#
+# STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+
+
+
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+   os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
@@ -140,6 +164,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 # Путь хранения картинок
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
